c1f1b47f1f2d8d44fa5827b868aa80c6ae33834b
jelambrar96 <jelambrar@gmail.com>
2025-12-23 11:29:08 -0500

    implement all methods of task and task_manager

--- tasks.py
+++ tasks.py
@@ -1,39 +1,56 @@
 """
 Gestión de tareas
 """
+from itertools import count
 
 
 class Task:
     def __init__(self, id, description):
         self.id = id
-        self.description = description
         self.is_completed = False
+        if description is None or description.strip() == "":
+            raise ValueError("Description cannot be empty")
+        self.description = description
 
 
 class TaskManager:
     def __init__(self):
         self._tasks = []
+        self._id_counter = count(1)
 
     def generate_id(self):
-        raise NotImplementedError("Subclasses must implement this method")
+        return str(next(self._id_counter))
 
     def add_task(self, task):
-        raise NotImplementedError("Subclasses must implement this method")
+        self._tasks.append(Task(self.generate_id(), task))
 
     def get_all_tasks(self):
-        raise NotImplementedError("Subclasses must implement this method")
+        return self._tasks
 
     def get_task_by_id(self, task_id):
-        raise NotImplementedError("Subclasses must implement this method")
+        for task in self._tasks:
+            if task.id == task_id:
+                return task
+        return None
 
     def update_task(self, task_id, new_description):
-        raise NotImplementedError("Subclasses must implement this method")
+        for task in self._tasks:
+            if task.id == task_id:
+                task.description = new_description
+                return
 
     def remove_task(self, task_id):
-        raise NotImplementedError("Subclasses must implement this method")
+        for i, task in enumerate(self._tasks):
+            if task.id == task_id:
+                self._tasks.pop(i)
+                self.send_removed_notification(task_id)
+                return
 
     def mark_task_as_completed(self, task_id):
-        raise NotImplementedError("Subclasses must implement this method")
+        for task in self._tasks:
+            if task.id == task_id:
+                task.is_completed = True
+                return
 
     def send_removed_notification(self, task_id):
-        raise NotImplementedError("Subclasses must implement this method")
+        pass
